7b6075933f0db4737554cf9626034ece18bf6033
JorisHeemskerk <joris_heemskerk@outlook.com>
2026-02-13 09:30:34 +0100

    add docstring and small fixes to main

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -2,28 +2,58 @@ import cv2
 import os
 import re
 import datetime
+import numpy as np
 
 from autmatic_corners import automatic_corner_detector
 from manual_corners import manual_corner_selector
 
 
 def detect_corners(folder: str, output_folder: str)-> None:
-    # read and sort the filenames, exclude hidden files
+    """
+    Detect the corners of the chessboards in every image in provided
+    folder. 
+    
+    Function first attempts to select the corners automatically. If this
+    fails, the user is prompted to select the four outer corners by hand
+    and then close the window(s). A user can right click to show where
+    a corner would be placed, in a separate window, as well as left 
+    click to place a definitive marker.
+    
+    :param folder: Folder in which input images are saved.
+    :type folder: str
+    :param output_folder: Folder to which output is saved. The corners
+    will be saved in a subdirectory called "corners". And the corner
+    points will also be saved in a subdirectory called "data"
+    :type output_folder: str
+    """
+    # Read and sort the filenames, exclude hidden files.
     filenames = sorted([
         filename for filename in os.listdir(folder) if filename[0] != "."
     ], key=lambda s: int(re.search(r'\d+', s).group()))
+    corners_all_images = []
     for filename in filenames:
         success, corners, img = automatic_corner_detector(folder + filename)
         if success == 0:
-            print(f"corners were not auto detected in image {filename}.")
+            print(
+                f"Corners were not automatically detected in image {filename}."
+                "\nPlease manually click on the four corners and then close "
+                "the image."
+            )
             success, corners, img = manual_corner_selector(folder + filename)
-        cv2.imwrite(output_folder + filename, img)
-    
+        corners_all_images.append(corners)
+        cv2.imwrite(output_folder + "corners/" + filename, img)
+
+    np.save(
+        output_folder + "data/corners_all_images.npy", 
+        np.array(corners_all_images)
+    )
 
 def main()-> None:
+    # Create output folder for the finished images.
     output_folder = "assignment_1/output/run_" + \
         f"{datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}/"
     os.mkdir(output_folder)
+
     detect_corners("assignment_1/data/", output_folder)
 
 if __name__ == "__main__":
