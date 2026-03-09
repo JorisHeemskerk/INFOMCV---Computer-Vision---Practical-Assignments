31391096b731fbefd12047c3835219cc62fc34e3
Timbaken <94841602+Timbaken@users.noreply.github.com>
2026-03-09 15:44:18 +0100

    first version

--- assignment_3/lenet.py
+++ assignment_3/lenet.py
@@ -3,14 +3,32 @@ from torch import nn
 import torch.nn.functional as F
 
 
-class Lenet5(nn.Module):
+class LeNet5(nn.Module):
     """
     The LeNet-5 model architecture but for color images.
     """
     def __init__(self):
-        super(Lenet5, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5, padding=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(6, 16, 5, padding=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(16, 120, 5, padding=2)
+        self.fc1 = nn.Linear(7680, 84)
+        self.fc2 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
+    
