1cc2c47d8d9502801d07342a961a5656067c53d7
Timbaken <94841602+Timbaken@users.noreply.github.com>
2026-03-10 17:15:14 +0100

    quick changes

--- assignment_3/lenet5_base.py
+++ assignment_3/lenet5_base.py
@@ -38,7 +38,7 @@ class LeNet5Base(nn.Module):
         function should be called manually after creating a class
         instance.
         """
-        for module in self.children():
+        for module in self.modules():
             print(module)
 
     def forward(self, x: torch.Tensor)-> torch.Tensor:
