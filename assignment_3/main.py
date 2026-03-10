e7c3bd67a93a02e26e87b5836b8390aac1b5ec46
Timbaken <94841602+Timbaken@users.noreply.github.com>
2026-03-10 16:42:47 +0100

    quick changes

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -41,8 +41,8 @@ def main()-> None:
     ####################################################################
     #                          Load the model.                         #
     ####################################################################
-    # model = LeNet5Base()
-    model = LeNet5MoreFeatureKernels(32)
+    model = LeNet5Base()
+    # model = LeNet5MoreFeatureKernels(32)
 
     model = model.to(DEVICE)
     ####################################################################
