fix for confusion matrix normalisation

assignment_3/main.py
@@ -28,7 +28,7 @@ def main()-> None:
     ####################################################################
     #                          Load the data.                          #
     ####################################################################
-    DATASET = datasets.CIFAR100
+    DATASET = datasets.CIFAR10
     FINETUNE = True
 
     train_dataset, val_dataset, test_dataset = load_datasets(
@@ -68,9 +68,9 @@ def main()-> None:
     ####################################################################
     #                     Set the hyperparemeters.                     #
     ####################################################################
-    N_EPOCHS = 2
+    N_EPOCHS = 50
     LEARNING_RATE = 0.001
-    K_FOLDS: int | None = 2
+    K_FOLDS: int | None = None
 
     OPTIMISER = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
     SCHEDULER = None
@@ -185,13 +185,13 @@ def main()-> None:
         test_labels, 
         test_predictions, 
         test_dataset.classes,
-        False
+        None
     )
     plot_confusion_matrix(
         test_labels, 
         test_predictions, 
         test_dataset.classes, 
-        True
+        'true'
     )
     
 

assignment_3/visualise.py
@@ -211,7 +211,7 @@ def plot_confusion_matrix(
     y_true: np.ndarray, 
     y_pred: np.ndarray, 
     class_names: list[str],
-    normalise: bool
+    normalise: str | None
 )-> None:
     """
     Plot a confusion matrix.
@@ -223,14 +223,14 @@ def plot_confusion_matrix(
     :param class_names: names of all classes.
     :type class_names: list[str]
     :param normalise: Normalise the class values.
-    :type normalise: bool
+    :type normalise: str | None
     """
     cm = confusion_matrix(y_true, y_pred, normalize=normalise)
     fig, ax = plt.subplots(figsize=(8, 6))
     sns.heatmap(
         cm,
         annot=True,
-        fmt="d",
+        fmt=".2f" if normalise else "d",
         cmap="Blues",
         xticklabels=class_names,
         yticklabels=class_names,