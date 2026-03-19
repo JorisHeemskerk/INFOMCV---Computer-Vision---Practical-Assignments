4dbf231583fca7ae87db3c01d0b8dd2d6ca29c9b
JorisHeemskerk <joris_heemskerk@outlook.com>
2026-03-19 12:20:23 +0100

    generalise cube unpacking

--- assignment_4/yolov1_loss.py
+++ assignment_4/yolov1_loss.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+from decode import unpack_cube
 
 class YOLOv1Loss(nn.Module):
     def __init__(self, lambda_coord: float, lambda_noobj: float):
@@ -17,21 +18,11 @@ class YOLOv1Loss(nn.Module):
           [x, y, w, h, conf, class0, class1]
            0  1  2  3   4      5       6
         """
-        # --- unpack predictions ---
-        pred_x    = y_hat[..., 0]
-        pred_y    = y_hat[..., 1]
-        pred_w    = y_hat[..., 2]
-        pred_h    = y_hat[..., 3]
-        pred_conf = y_hat[..., 4]
-        pred_cls  = y_hat[..., 5:]   # (N, S, S, 2)
+        pred_x, pred_y, pred_w, pred_h, pred_conf, pred_cls = unpack_cube(
+            y_hat
+        )
+        true_x, true_y, true_w, true_h, true_conf, true_cls = unpack_cube(y)
 
-        # --- unpack ground truth ---
-        true_x    = y[..., 0]
-        true_y    = y[..., 1]
-        true_w    = y[..., 2]
-        true_h    = y[..., 3]
-        true_conf = y[..., 4]        # 1 if object present, else 0
-        true_cls  = y[..., 5:]       # (N, S, S, 2)
 
         obj_mask  = true_conf        # 1_ij^obj  — shape (N, S, S)
         noobj_mask = 1.0 - obj_mask  # 1_ij^noobj
